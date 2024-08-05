8b7e977f5fcafdcab15db45484f4381dc011cb7d
Humbat <humbet.alizade13@gmail.com>
2024-08-05 12:59:29 +0400

    Spring-Boot-StudentCourseProject

--- src/main/java/org/book/studentcourse/controller/EmailController.java
+++ src/main/java/org/book/studentcourse/controller/EmailController.java
@@ -19,6 +19,8 @@ public class EmailController {
 
 
 
+
+
     @RequestMapping("/send/simple")
     public void sendEmail(@RequestBody EmailRequestDto email){
         emailService.sendEmail(email);
